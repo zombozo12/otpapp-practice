6d16025b0174bc50c0988900d9153ad7c0fb1ed2
wigunarrr <wigunaramadhan1998@gmail.com>
2021-11-23 20:38:45 +0700

    added rate limiter

--- router/api.go
+++ router/api.go
@@ -2,11 +2,16 @@ package router
 
 import (
 	"github.com/go-chi/chi"
+	"github.com/go-chi/chi/middleware"
+	"github.com/go-chi/httprate"
 	http2 "otpapp-native/http"
+	"time"
 )
 
 func Init() *chi.Mux {
 	rt := chi.NewRouter()
+	rt.Use(middleware.Throttle(1))
+	rt.Use(httprate.LimitAll(5, 1*time.Minute))
 	rt.Route("/otp", func(r chi.Router) {
 		r.Post("/request", http2.PhoneRequest)
 		r.Post("/validate", http2.PhoneValidate)
